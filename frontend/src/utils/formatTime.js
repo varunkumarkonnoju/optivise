bcd4d04191f21e13d92501c74b65b0d99c1efb1d
Varun Kumar Konnoju <varunkumarkonnoju@gmail.com>
2026-04-29 10:57:47 -0500

    fix timezone - convert UTC to user local time

--- frontend/src/utils/formatTime.js
+++ frontend/src/utils/formatTime.js
@@ -2,7 +2,9 @@
 export function formatLocalTime(isoString) {
   if (!isoString) return 'Just now'
   try {
-    const date = new Date(isoString)
+    // Add Z suffix to tell JavaScript this is UTC time
+    const utcString = isoString.endsWith('Z') ? isoString : isoString + 'Z'
+    const date = new Date(utcString)
     if (isNaN(date.getTime())) return 'Just now'
     return date.toLocaleString([], {
       month: 'short',
